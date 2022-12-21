0e52c06a228d4e6d3ba9c22dd42cbfd1467db800
dsaglam94 <d.saglam@outlook.fr>
2022-12-21 14:30:36 +0300

    creating prisma models

---
+++ src/models/User.ts
@@ -0,0 +1,22 @@
+import { builder } from "../schema/builder";
+import { prisma } from "../db/db";
+
+builder.prismaObject("User", {
+  fields: (t) => ({
+    id: t.exposeID("id"),
+    name: t.exposeString("name"),
+    messages: t.relation("messages"),
+    createdAt: t.expose("createdAt", {
+      type: "Date",
+    }),
+  }),
+});
+
+builder.queryField("users", (t) =>
+  t.prismaField({
+    type: ["User"],
+    resolve: async (query, root, args, ctx, info) => {
+      return prisma.user.findMany({ ...query });
+    },
+  })
+);
