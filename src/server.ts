52c187cfceb46287bb83c81ab80de3cc0e7f00a3
dsaglam94 <d.saglam@outlook.fr>
2022-12-21 14:31:25 +0300

    adding pothos package and implementing builder schema

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,25 @@
-console.log("hello!");
+import { createSchema, createYoga } from "graphql-yoga";
+import { createServer } from "node:http";
+
+const port = Number(process.env.API_PORT) || 4000;
+
+const yoga = createYoga({
+  schema: createSchema({
+    typeDefs: /* GraphQL */ `
+      type Query {
+        hello: String
+      }
+    `,
+    resolvers: {
+      Query: {
+        hello: () => "Hello from Yoga!",
+      },
+    },
+  }),
+});
+
+const server = createServer(yoga);
+
+server.listen(4000, () => {
+  console.log(`🚀 GraphQL Server ready at http://localhost:${port}/graphql`);
+});
